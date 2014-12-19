11c0b45643df9d2f615c2ac58b45cf8e624a912a
Gurvir <harjap.singh.pabla@gmail.com>
2014-12-19 20:13:49 +0000

    Adding the first hello world lesson

---
+++ lesson1-helloWorld/helloworld.cpp
@@ -0,0 +1,16 @@
+// We include iostream so that we can use std::cin for input
+// and std::cout for output
+#include <iostream>
+
+// By specifying using namespace we will not have to
+// keep writing std::cin and std::cout
+using namespace std;
+
+
+// All C++ programs begin with a main as follows
+int main()
+{
+	// We use cout to "OUTPUT" to the screen
+	// in later lessons we will use cin to get "INPUT" from the users
+	cout << "Hello World - Gurvir has arrived"<<endl;
+}
